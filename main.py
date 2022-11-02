b8474bfd89b1c62a035f21ec6da2dcbc0a0e5d1d
dahouo50@gmail.com <Arnaud Ahouo>
2022-11-02 15:29:02 -0400

    Instead of execute insert. Generate csv ans load it

--- main.py
+++ main.py
@@ -2,36 +2,15 @@ import json
 import random
 
 import pymysql
-
-# read the config.json file
 import generator
 
+# read the config.json file
 with open("config.json", "r") as f:
     config = json.loads(f.read())
 
 
 # Generate random data for mysql database
 def generate_data():
-    # Connect to the database
-    connection = pymysql.connect(host=config.get("MMYSQL_DATABASE_HOST"),
-                                 user=config.get("MYSQL_DATABASE_USER"),
-                                 password=config.get("MYSQL_DATABASE_PASSWORD"),
-                                 db=config.get("MYSQL_DATABASE_NAME"),
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-
-    try:
-        with connection.cursor() as cursor:
-            # Create a new record
-            for i in range(config.get("NUMBER_OF_ROWS")):
-                sql = build_insert_statement()
-                cursor.execute(sql)
-                connection.commit()
-    finally:
-        connection.close()
-
-
-def build_insert_statement():
     columns = config.get("COLUMNS")
     columns_str = ""
     values_str = ""
@@ -44,9 +23,28 @@ def build_insert_statement():
             if column.get("UNIQUE"):
                 value = str(value) + str(random.randint(0, 9999999999))
             values_str += "'" + value + "', " if isinstance(value, str) else str(value) + ", "
-    columns_str = "(" + columns_str[:-2] + ")"
-    values_str = "VALUES(" + values_str[:-2] + ")"
-    return "INSERT INTO " + config.get("MYSQL_DATABASE_TABLE_NAME") + columns_str + " " + values_str
+
+    with open("data.csv", "w") as f:
+        f.write(columns_str[:-2] + "")
+        for i in range(config.get("NUMBER_OF_ROWS")):
+            f.write(values_str[:-2] + "")
+
+    connection = pymysql.connect(host=config.get("MMYSQL_DATABASE_HOST"),
+                                 user=config.get("MYSQL_DATABASE_USER"),
+                                 password=config.get("MYSQL_DATABASE_PASSWORD"),
+                                 db=config.get("MYSQL_DATABASE_NAME"),
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+
+    try:
+        with connection.cursor() as cursor:
+            # Create a new record
+            sql = "LOAD DATA LOCAL INFILE 'data.csv' INTO TABLE " + config.get("MYSQL_DATABASE_TABLE_NAME") + \
+                  " FIELDS TERMINATED BY ',' LINES TERMINATED BY '' IGNORE 1 LINES"
+            cursor.execute(sql)
+            connection.commit()
+    finally:
+        connection.close()
 
 
 # Press the green button in the gutter to run the script.
