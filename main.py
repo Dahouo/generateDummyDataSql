1eb649d40d28b55dba08a89ca05592639a4b32c5
dahouo50@gmail.com <Arnaud Ahouo>
2022-11-02 20:14:10 -0400

    Add ReadMe. Small optimisations

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import csv
 import json
+import os
 import random
 
 import pymysql
@@ -12,23 +14,15 @@ with open("config.json", "r") as f:
 # Generate random data for mysql database
 def generate_data():
     columns = config.get("COLUMNS")
-    columns_str = ""
-    values_str = ""
+    columns_str = list()
     for column in columns:
-        columns_str += column.get("NAME") + ", "
-        if column.get("TYPE") == "ENUM":
-            values_str += random.choice(column.get("ALLOWED_VALUES")) + ", "
-        else:
-            value = generator.generate(column.get("TYPE"))
-            if column.get("UNIQUE"):
-                value = str(value) + str(random.randint(0, 9999999999))
-            values_str += "'" + value + "', " if isinstance(value, str) else str(value) + ", "
+        columns_str.append(column.get("NAME"))
 
-    with open("data.csv", "w") as f:
-        f.write(columns_str[:-2] + "")
+    with open("data.csv", "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(columns_str)
         for i in range(config.get("NUMBER_OF_ROWS")):
-            f.write(values_str[:-2] + "")
-
+            writer.writerow(build_values(columns))
     connection = pymysql.connect(host=config.get("MMYSQL_DATABASE_HOST"),
                                  user=config.get("MYSQL_DATABASE_USER"),
                                  password=config.get("MYSQL_DATABASE_PASSWORD"),
@@ -38,15 +32,31 @@ def generate_data():
 
     try:
         with connection.cursor() as cursor:
-            # Create a new record
             sql = "LOAD DATA LOCAL INFILE 'data.csv' INTO TABLE " + config.get("MYSQL_DATABASE_TABLE_NAME") + \
-                  " FIELDS TERMINATED BY ',' LINES TERMINATED BY '' IGNORE 1 LINES"
+                  " FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES"
             cursor.execute(sql)
             connection.commit()
     finally:
         connection.close()
+        os.remove("data.csv")
 
 
-# Press the green button in the gutter to run the script.
+def build_values(columns):
+    values_str = list()
+    for column in columns:
+        if column.get("TYPE") == "ENUM":
+            values_str.append(random.choice(column.get("ALLOWED_VALUES")))
+        else:
+            value = generator.generate(column.get("TYPE"))
+            if column.get("UNIQUE"):
+                value = str(value) + str(random.randint(0, 9999999999))
+            if column.get("MIN_LENGTH") and len(value) < column.get("MIN_LENGTH"):
+                value = value + str(random.randint(0, 9)) * (column.get("MIN_LENGTH") - len(value))
+            if column.get("MAX_LENGTH") and len(value) > column.get("MAX_LENGTH"):
+                value = value[:column.get("MAX_LENGTH")]
+            values_str.append(value)
+    return values_str
+
+
 if __name__ == '__main__':
     generate_data()
